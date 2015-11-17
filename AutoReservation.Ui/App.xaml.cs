b0cd5f4f60de9f5cb33a8237a9620a560cfe4500
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2015-11-17 09:00:22 +0100

    Initial commit

---
+++ AutoReservation.Ui/App.xaml.cs
@@ -0,0 +1,83 @@
+<<<<<<< HEAD
+﻿using System.Windows;
+using AutoReservation.Ui.ViewModels;
+using Ninject;
+
+namespace AutoReservation.Ui
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IKernel kernel;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            kernel = LoadNinject();
+
+            var vm = kernel.Get<MainWindowViewModel>();
+            var view = new MainWindow();
+            view.DataContext = vm;
+            vm.Init();
+            this.MainWindow = view;
+            this.MainWindow.Show();
+        }
+
+        private IKernel LoadNinject()
+        {
+            var kernel = new StandardKernel(new AutoReservationModule());
+            kernel.Load("Dependencies.Ninject.xml");
+            return kernel;
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            kernel.Dispose();
+            base.OnExit(e);
+        }
+    }
+}
+=======
+﻿using System.Windows;
+using AutoReservation.Ui.ViewModels;
+using Ninject;
+
+namespace AutoReservation.Ui
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private IKernel kernel;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            kernel = LoadNinject();
+
+            var vm = kernel.Get<MainWindowViewModel>();
+            var view = new MainWindow();
+            view.DataContext = vm;
+            vm.Init();
+            this.MainWindow = view;
+            this.MainWindow.Show();
+        }
+
+        private IKernel LoadNinject()
+        {
+            var kernel = new StandardKernel(new AutoReservationModule());
+            kernel.Load("Dependencies.Ninject.xml");
+            return kernel;
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            kernel.Dispose();
+            base.OnExit(e);
+        }
+    }
+}
+>>>>>>> 4edd2da... FIX: add rest of project
