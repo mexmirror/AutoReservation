68b82a33c3d935290a0a1360ab04a86003001601
ValisStigma <rafael_krucker@hotmail.com>
2015-11-17 13:25:20 +0100

    Added first implementation of business layer

--- AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
+++ AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
@@ -1,14 +1,129 @@
-﻿namespace AutoReservation.BusinessLayer
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Threading.Tasks;
+using AutoReservation.Dal;
+
+namespace AutoReservation.BusinessLayer
 {
     public class AutoReservationBusinessComponent
     {
+        public static Task<List<Auto>> GetCars(AutoReservationEntities context)
+        {
+            return context.Autoes.ToListAsync();
+        }
 
-        //private static void HandleDbConcurrencyException<T>(AutoReservationEntities context, T original) where T : class
-        //{
-        //    var databaseValue = context.Entry(original).GetDatabaseValues();
-        //    context.Entry(original).CurrentValues.SetValues(databaseValue);
+        public static Task<Auto> GetCar(AutoReservationEntities context, int id)
+        {
+            return context.Autoes.FindAsync(id);
+        }
 
-        //    throw new LocalOptimisticConcurrencyException<T>(string.Format("Update {0}: Concurrency-Fehler", typeof(T).Name), original);
-        //}
+        public static void InsertCar(AutoReservationEntities context, Auto car)
+        {
+            context.Autoes.Add(car);
+            context.SaveChangesAsync();
+        }
+
+        public static void UpdateCar(AutoReservationEntities context, Auto modified, Auto original)
+        {
+            context.Autoes.Attach(modified);
+            try
+            {
+                context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                HandleDbConcurrencyException(context, original);
+            }
+        }
+
+        public static void DeleteCar(AutoReservationEntities context, Auto car)
+        {
+            context.Autoes.Attach(car);
+            context.Autoes.Remove(car);
+            context.SaveChangesAsync();
+        }
+
+        public static Task<List<Reservation>> GetReservations(AutoReservationEntities context)
+        {
+            return context.Reservations.ToListAsync();
+        }
+
+        public static Task<Reservation> GetReservation(AutoReservationEntities context, int id)
+        {
+            return context.Reservations.FindAsync(id);
+        }
+
+        public void InsertReservation(AutoReservationEntities context, Reservation reservation)
+        {
+            context.Reservations.Add(reservation);
+            context.SaveChangesAsync();
+        }
+
+        public void UpdateReservation(AutoReservationEntities context, Reservation modified, Reservation original)
+        {
+            context.Reservations.Attach(modified);
+            try
+            {
+                context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                HandleDbConcurrencyException(context, original);
+            }
+        }
+
+        public void DeleteReservation(AutoReservationEntities context, Reservation reservation)
+        {
+            context.Reservations.Attach(reservation);
+            context.Reservations.Remove(reservation);
+            context.SaveChangesAsync();
+        }
+
+        public static Task<List<Kunde>> GetCustomers(AutoReservationEntities context)
+        {
+            return context.Kundes.ToListAsync();
+        }
+
+        public static Task<Kunde> GetCustomer(AutoReservationEntities context, int id)
+        {
+            return context.Kundes.FindAsync(id);
+        }
+
+        public static void InsertCustomer(AutoReservationEntities context, Kunde customer)
+        {
+            context.Kundes.Add(customer);
+            context.SaveChangesAsync();
+        }
+
+        public static void UpdateCustomer(AutoReservationEntities context, Kunde modified, Kunde original)
+        {
+            context.Kundes.Attach(modified);
+            try
+            {
+                context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                HandleDbConcurrencyException(context, original);
+            }
+
+        }
+
+        public static void DeleteCustomer(AutoReservationEntities context, Kunde customer)
+        {
+            context.Kundes.Attach(customer);
+            context.Kundes.Remove(customer);
+            context.SaveChangesAsync();
+        }
+
+        private static void HandleDbConcurrencyException<T>(AutoReservationEntities context, T original) where T : class
+        {
+            var databaseValue = context.Entry(original).GetDatabaseValues();
+            context.Entry(original).CurrentValues.SetValues(databaseValue);
+
+            throw new LocalOptimisticConcurrencyException<T>($"Update {typeof (T).Name}: Concurrency-Fehler", original);
+        }
     }
 }
--- AutoReservation.Dal/AutoReservationEntities.Designer.cs
+++ AutoReservation.Dal/AutoReservationEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\olivi\Development\AutoReservation\AutoReservation.Dal\AutoReservationEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\rafael\programming\AutoReservation\AutoReservation.Dal\AutoReservationEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
