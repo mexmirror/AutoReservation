c5aa3a740d0a0587febf147bd679f3a105ec64e7
Oliviero Chiodo <oliviero.chiodo@gmail.com>
2015-12-15 12:07:13 +0100

    MOD: bl

--- AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
+++ AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
@@ -1,14 +1,133 @@
-﻿namespace AutoReservation.BusinessLayer
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoReservation.Dal;
+
+namespace AutoReservation.BusinessLayer
 {
     public class AutoReservationBusinessComponent
     {
+        private static readonly AutoReservationEntities _context = new AutoReservationEntities();
 
-        //private static void HandleDbConcurrencyException<T>(AutoReservationEntities context, T original) where T : class
-        //{
-        //    var databaseValue = context.Entry(original).GetDatabaseValues();
-        //    context.Entry(original).CurrentValues.SetValues(databaseValue);
+        public static Task<List<Auto>> GetCars()
+        {
+            return _context.Autos.ToListAsync();
+        }
 
-        //    throw new LocalOptimisticConcurrencyException<T>(string.Format("Update {0}: Concurrency-Fehler", typeof(T).Name), original);
-        //}
+        public static Task<Auto> GetCar(int id)
+        {
+            return _context.Autos.FindAsync(id);
+        }
+
+        public static void InsertCar(Auto car)
+        {
+            _context.Autos.Add(car);
+            _context.SaveChangesAsync();
+        }
+
+        public static void UpdateCar(Auto modified, Auto original)
+        {
+            _context.Autos.Attach(modified);
+            try
+            {
+                _context.SaveChangesAsync();
+            }
+            #pragma warning disable CS0168
+            catch (DBConcurrencyException e)
+            {
+                HandleDbConcurrencyException(_context, original);
+            }
+        }
+
+        public static void DeleteCar(Auto car)
+        {
+            _context.Autos.Attach(car);
+            _context.Autos.Remove(car);
+            _context.SaveChangesAsync();
+        }
+
+        public static Task<List<Reservation>> GetReservations()
+        {
+            return _context.Reservations.ToListAsync();
+        }
+
+        public static Task<Reservation> GetReservation(int id)
+        {
+            return _context.Reservations.FindAsync(id);
+        }
+
+        public static void InsertReservation(Reservation reservation)
+        {
+            _context.Reservations.Attach(reservation);
+        }
+
+        public static void UpdateReservation(Reservation modified, Reservation original)
+        {
+            _context.Reservations.Attach(modified);
+            try
+            {
+                _context.SaveChangesAsync();
+            }
+            #pragma warning disable CS0168
+            catch (DBConcurrencyException e)
+            {
+                HandleDbConcurrencyException(_context, original);
+            }
+        }
+
+        public static void DeleteReservation(Reservation reservation)
+        {
+            _context.Reservations.Attach(reservation);
+            _context.Reservations.Remove(reservation);
+            _context.SaveChangesAsync();
+        }
+
+        public static Task<List<Kunde>> GetCustomers()
+        {
+            return _context.Kundes.ToListAsync();
+        }
+
+        public static Task<Kunde> GetCustomer(int id)
+        {
+            return _context.Kundes.FindAsync(id);
+        }
+
+        public static void InsertCustomer(Kunde customer)
+        {
+            _context.Kundes.Attach(customer);
+            _context.SaveChangesAsync();
+
+        }
+
+        public static void UpdateCustomer(Kunde modified, Kunde original)
+        {
+            _context.Kundes.Attach(modified);
+            try
+            {
+                _context.SaveChangesAsync();
+            }
+            #pragma warning disable CS0168
+            catch (DBConcurrencyException e)
+            {
+                HandleDbConcurrencyException(_context, original);
+            }
+        }
+
+        public static void DeleteCustomer(Kunde customer)
+        {
+            _context.Kundes.Attach(customer);
+            _context.Kundes.Remove(customer);
+            _context.SaveChangesAsync();
+        }
+        private static void HandleDbConcurrencyException<T>(AutoReservationEntities context, T original) where T : class
+        {
+            var databaseValue = context.Entry(original).GetDatabaseValues();
+            context.Entry(original).CurrentValues.SetValues(databaseValue);
+            throw new LocalOptimisticConcurrencyException<T>(string.Format("Update {0}: Concurrency-Fehler", typeof(T).Name), original);
+        }
     }
 }
--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,7 +1,26 @@
-﻿namespace AutoReservation.Common.Interfaces
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoReservation.Common.DataTransferObjects;
+
+namespace AutoReservation.Common.Interfaces
 {
     public interface IAutoReservationService
     {
-        
+        Task<List<AutoDto>> GetCars();
+        Task<AutoDto> GetCar(int id);
+        void InsertCar(AutoDto car);
+        void UpdateCar(AutoDto modified, AutoDto original);
+        void DeleteCar(AutoDto car);
+        Task<List<ReservationDto>> GetReservations();
+        Task<ReservationDto> GetReservation(int id);
+        void InsertReservation(ReservationDto reservation);
+        void UpdateReservation(ReservationDto modified, ReservationDto original);
+        void DeleteReservation(ReservationDto reservation);
+
+        Task<List<KundeDto>> GetCustomers();
+        Task<KundeDto> GetCustomer(int id);
+        void InsertCustomer(KundeDto customer);
+        void UpdateCustomer(KundeDto modified, KundeDto original);
+        void DeleteCustomer(KundeDto customer);
     }
 }
